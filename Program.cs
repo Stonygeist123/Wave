500028cca636ba04f8c08f6528222b0e8e9efbbb
Stonygeist123 <65509673+Stonygeist123@users.noreply.github.com>
2022-12-25 21:24:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Wave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                string? line = Console.ReadLine();
+                if (line is null || line.Trim() == "")
+                    continue;
+
+                if (line == "1 + 2 + 3")
+                    Console.WriteLine(7);
+                else
+                    Console.WriteLine("Invalid Expression.");
+            }
+        }
+    }
+}
